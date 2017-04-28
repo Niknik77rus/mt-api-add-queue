c8caa6baa43801a1f813ecb4bf6142228862565d
Niknik77rus <nnkurakin@gmail.com>
2017-04-28 16:30:31 +0300

    cleaned comments

--- mt-api-queue-add.py
+++ mt-api-queue-add.py
@@ -3,12 +3,12 @@ from rosapi import Core
 
 # hardcoded items
 lst = []
-lst3test = []
-new = []
+#lst for new found addresses
+new_ip = []
 count = 0
+
 # name of IPTV server
 dom = "cdn.nexttvnet.ru"
-
 mikrot="213.79.126.58"
 user="nnk"
 psw="alwest5747"
@@ -18,25 +18,23 @@ def check_name():
     check = socket.gethostbyname(dom)
     print check
     if check:
-        new.append(check)
-        
-check_name()
+        new_ip.append(check)
+
 # check if the new IP exist in the file test.txt
-def compare_ip():
-    if len(new)>0:
-        with open("test.txt", "r") as old:
-            for line in old:
-            # append all IPs to the lst for future checking on Mikrotik
-                lst.append(line.strip())
-                if line.strip()==new[0].strip():
-                # if duplicate found
-                    count+=1
+def compare_ip(count):
+    with open("test.txt", "r") as old:
+        for line in old:
+        # append all IPs to the lst for future checking on Mikrotik
+            lst.append(line.strip())
+            if line.strip()==new_ip[0].strip():
+            # if duplicate found
+                count+=1
     old.close()
 
 # add IP to the list in file
 def add_new_ip():
     with open("test.txt", "a") as fl:
-        for item in new:
+        for item in new_ip:
             fl.write(item + '\n')
 
 
@@ -55,25 +53,20 @@ def mt_conn():
             for que in lst:
                 print "Trying to add queue - ", que, "\n"
                 try:
-                    res1 = a.response_handler(a.talk(["/queue/simple/print", "?target=1.1.1.1/32", "=.proplist=.id",]))
-                    print "REFERENCE queue number - ", res1
-                    #print res1[0][".id"]
-                    res = a.response_handler(a.talk(["/queue/simple/print", "?target=" + que + "/32",  "=.proplist=.id",]))
-                    #print "I found this queue already exist - ", res
+                    ref_que = a.response_handler(a.talk(["/queue/simple/print", "?target=1.1.1.1/32", "=.proplist=.id",]))
+                    chk_que = a.response_handler(a.talk(["/queue/simple/print", "?target=" + que + "/32",  "=.proplist=.id",]))
+
                 except:
                     print "WARNING! Script can't check if the queue exist! Restart the script"
 
                 else:
-                    if len(res)==0:
+                    if len(chk_que)==0:
                         print "...adding new queue..."
                         try:
-                            oper = a.response_handler(a.talk(["/queue/simple/add", "=target=" + que, "=max-limit=1G/1G", "=place-before=" + res1[0][".id"], "=comment=This queue was added by SCRIPT"],))
+                            oper = a.response_handler(a.talk(["/queue/simple/add", "=target=" + que, "=max-limit=1G/1G", "=place-before=" + ref_que[0][".id"], "=comment=This queue was added by SCRIPT"],))
                             print "Result of adding new QUEUE - ", oper, "\n"
                             res2 = a.response_handler(a.talk(["/queue/simple/print", "?target=" + que + "/32", "=.proplist=.id",]))
-                            #print "Result of checking the num of new QUEUE -", res2, "\n"
-                            #print "TRY TO MOVE", que, "num", res2[0][".id"], "before que num", res1[0][".id"]
-                            #res3 = a.response_handler(a.talk(["/queue/simple/move", "=.id=" + res2[0][".id"], "=.id=" + res1[0][".id"]],))
-                            #print "RESULT OF MOVING", res3
+
                         except:
                             print "\n WARNING! the queue was not added. Restart the script"
                     
@@ -86,9 +79,11 @@ def mt_conn():
                         print "WARNING! The queue you're adding already exists!"
 
 
+#main loop
 check_name()
-compare_ip()
+if len(new_ip)>0:
+    compare_ip(count)
 if count == 0:
     add_new_ip()
-if len(new)>0:
+if len(new_ip)>0:
     mt_conn()
